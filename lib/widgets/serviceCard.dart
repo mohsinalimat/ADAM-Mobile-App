c861db44c214639dc5d01053c6d3596ce12f3eaf
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2021-05-30 13:56:46 +0500

    Widgets added + FAB

---
+++ lib/widgets/serviceCard.dart
@@ -0,0 +1,173 @@
+import 'package:adam/views/services/serviceSubscriptionView.dart';
+import 'package:flutter/material.dart';
+
+class ServiceCard extends StatelessWidget {
+  const ServiceCard(
+      {Key key,
+      @required this.serviceIcon,
+      @required this.serviceName,
+      @required this.serviceDescription,
+      @required this.servicePrice,
+      @required this.gradientColors,
+      this.addFavorite,
+      this.isFavorite = false})
+      : super(key: key);
+
+  final IconData serviceIcon;
+  final String serviceName;
+  final String serviceDescription;
+  final String servicePrice;
+  final List<Color> gradientColors;
+  final Function addFavorite;
+  final bool isFavorite;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: gradientColors,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    serviceIcon,
+                    color: Colors.white,
+                    size: 45.0,
+                  ),
+                  SizedBox(width: 8.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          serviceName,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Campaign",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "4.7",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 13.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            for (int i = 0; i < 4; i++)
+                              Icon(
+                                Icons.star,
+                                color: Colors.yellow,
+                                size: 15.0,
+                              ),
+                            Icon(
+                              Icons.star_half,
+                              color: Colors.yellow,
+                              size: 15.0,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  GestureDetector(
+                    onTap: addFavorite,
+                    child: Icon(
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              serviceDescription,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "\$$servicePrice/",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "month",
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+                FloatingActionButton(
+                  heroTag: serviceIcon.toString(),
+                  elevation: 1.0,
+                  backgroundColor: gradientColors[0],
+                  onPressed: () => Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      reverseTransitionDuration: Duration(seconds: 1),
+                      transitionDuration: Duration(seconds: 1),
+                      transitionsBuilder: (context, ani1, ani2, child) {
+                        return FadeTransition(
+                          opacity: ani1,
+                          child: child,
+                        );
+                      },
+                      pageBuilder: (context, anime1, anime2) =>
+                          ServiceSubscriptionView(
+                        serviceName: serviceName,
+                        iconData: serviceIcon,
+                        colorTheme: gradientColors[0],
+                        serviceDesc:
+                            "With Facebook marketing campaign you can now grow your business, market your brand or any organization with number of inbox messages, posts in multiple groups and much more!\n\nSo, subscribe to your favorite serivce and get started now!",
+                      ),
+                    ),
+                  ),
+                  mini: true,
+                  child: Icon(
+                    Icons.arrow_forward,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
