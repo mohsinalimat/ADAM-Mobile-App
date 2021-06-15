87997bf486a489196b1f785bc9fd9fd5a462a51e
m-hamzashakeel <hamza.6.shakeel@gmail.com>
2021-06-15 12:14:30 +0500

    help complete + backend left

--- lib/views/settings/help/reportProblem.dart
+++ lib/views/settings/help/reportProblem.dart
@@ -3,6 +3,7 @@ import 'package:adam/controller/themeController/themeProvider.dart';
 import 'package:adam/widgets/customBtn.dart';
 import 'package:adam/widgets/customTextField.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class ReportProblemView extends StatefulWidget {
@@ -33,69 +34,76 @@ class _ReportProblemViewState extends State<ReportProblemView> {
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 20.0),
-                CustomTextField(
-                  textEditingController: _subjectFieldController,
-                  textInputAction: TextInputAction.next,
-                  textInputType: TextInputType.text,
-                  hintText: "Problem title/subject",
-                  icon: Icons.subject_rounded,
-                  validatorFtn: (value) {
-                    if (value == null) {
-                      return "Subject cannot be empty!";
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 15.0),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.89,
-                  child: TextFormField(
-                    maxLines: 5,
-                    controller: _detailsFieldController,
-                    keyboardType: TextInputType.multiline,
-                    textInputAction: TextInputAction.newline,
-                    decoration: InputDecoration(
-                      hintText: "Describe your problem in details",
-                      hintStyle: Theme.of(context).textTheme.caption,
-                      fillColor: _themeProvider.darkTheme
-                          ? Colors.black12
-                          : Colors.grey[200],
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.transparent)),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red[700]),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red[700]),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 20.0),
+                  SvgPicture.asset(
+                    'assets/problem.svg',
+                    height: 130.0,
+                  ),
+                  const SizedBox(height: 35.0),
+                  CustomTextField(
+                    textEditingController: _subjectFieldController,
+                    textInputAction: TextInputAction.next,
+                    textInputType: TextInputType.text,
+                    hintText: "Problem title/subject",
+                    icon: Icons.subject_rounded,
+                    validatorFtn: (value) {
+                      if (value == null) {
+                        return "Subject cannot be empty!";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 15.0),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.89,
+                    child: TextFormField(
+                      maxLines: 5,
+                      controller: _detailsFieldController,
+                      keyboardType: TextInputType.multiline,
+                      textInputAction: TextInputAction.newline,
+                      decoration: InputDecoration(
+                        hintText: "Describe your problem in details",
+                        hintStyle: Theme.of(context).textTheme.caption,
+                        fillColor: _themeProvider.darkTheme
+                            ? Colors.black12
+                            : Colors.grey[200],
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent)),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20.0),
-                CustomButton(
-                  btnWidth: 100.0,
-                  btnHeight: 40.0,
-                  btnOnPressed: () {},
-                  btnColor: kPrimaryBlueColor,
-                  btnText: Text(
-                    "Submit",
-                    style: kBtnTextStyle,
+                  const SizedBox(height: 30.0),
+                  CustomButton(
+                    btnWidth: 100.0,
+                    btnHeight: 40.0,
+                    btnOnPressed: () {},
+                    btnColor: kPrimaryBlueColor,
+                    btnText: Text(
+                      "Submit",
+                      style: kBtnTextStyle,
+                    ),
                   ),
-                ),
-                TextButton(
-                    onPressed: () => Navigator.pushNamed(context, "/faq"),
-                    child: Text("View FAQs"))
-              ],
+                  TextButton(
+                      onPressed: () => Navigator.pushNamed(context, "/faq"),
+                      child: Text("View FAQs"))
+                ],
+              ),
             ),
           ),
         ),
