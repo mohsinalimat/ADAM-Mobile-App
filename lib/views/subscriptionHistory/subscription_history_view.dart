0c381cc6bb8f580154ae58ace306245bbaae43d9
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2021-11-20 16:33:46 +0500

    new receipt added

--- lib/views/subscriptionHistory/subscription_history_view.dart
+++ lib/views/subscriptionHistory/subscription_history_view.dart
@@ -1,7 +1,11 @@
 import 'package:adam/animations/bottomAnimation.dart';
+import 'package:adam/constants.dart';
+import 'package:adam/controller/service_controller.dart';
 import 'package:adam/utils/custom_snackbar.dart';
+import 'package:adam/utils/main_imports.dart';
 import 'package:adam/widgets/subscriptionHistoryCard.dart';
 import 'package:flutter/material.dart';
+import 'package:progress_indicators/progress_indicators.dart';
 
 class SubscriptionHistoryView extends StatefulWidget {
   @override
@@ -10,39 +14,6 @@ class SubscriptionHistoryView extends StatefulWidget {
 }
 
 class _SubscriptionHistoryViewState extends State<SubscriptionHistoryView> {
-  final List<SubscriptionHistoryCard> cards = [
-    SubscriptionHistoryCard(
-      date: '12-10-2021',
-      isPrem: true,
-      serviceName: 'Instagram Marketing',
-    ),
-    SubscriptionHistoryCard(
-      date: '01-11-2021',
-      isPrem: true,
-      serviceName: 'Twitter Marketing',
-    ),
-    SubscriptionHistoryCard(
-      date: '12-01-2022',
-      isPrem: false,
-      serviceName: 'Email Marketing',
-    ),
-    SubscriptionHistoryCard(
-      date: '12-08-2022',
-      isPrem: false,
-      serviceName: 'LinkedIn Marketing',
-    ),
-    SubscriptionHistoryCard(
-      date: '11-02-2021',
-      isPrem: true,
-      serviceName: 'Instagram Marketing',
-    ),
-    SubscriptionHistoryCard(
-      date: '18-07-2021',
-      isPrem: false,
-      serviceName: 'Twitter Marketing',
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     final _textTheme = Theme.of(context).textTheme;
@@ -50,12 +21,29 @@ class _SubscriptionHistoryViewState extends State<SubscriptionHistoryView> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                BackButton(
-                  onPressed: () => Navigator.pop(context),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    BackButton(
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    InkWell(
+                      onTap: _clearAllHistory,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: const Text(
+                          "Clear history",
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 10.0),
                 Text(
@@ -65,65 +53,120 @@ class _SubscriptionHistoryViewState extends State<SubscriptionHistoryView> {
                 SizedBox(height: 5.0),
                 Text("We care about your subscription!"),
                 const SizedBox(height: 25.0),
-                ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: cards.length,
-                    itemBuilder: (context, index) {
-                      if (cards.length == 0) {
-                        return Center(
-                          child: Text("No Subscriptions found!"),
+                FutureBuilder(
+                  future: ServiceController().viewSubscriptionHistory(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasData) {
+                      if (snapshot.data.subscriptions.length == 0) {
+                        return Container(
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          child: Center(
+                            child: Text('No History Found!'),
+                          ),
                         );
                       }
-                      return WidgetAnimator(
-                        child: Dismissible(
-                          key: UniqueKey(),
-                          child: cards[index],
-                          direction: DismissDirection.endToStart,
-                          onDismissed: (direction) {
-                            if (direction == DismissDirection.endToStart) {
-                              setState(() {
-                                cards.removeAt(index);
-                              });
-                              customSnackBar(
-                                  context,
-                                  Colors.red[700],
-                                  Row(
-                                    children: [
-                                      const Icon(Icons.delete_rounded,
-                                          color: Colors.white),
-                                      const SizedBox(width: 8.0),
-                                      Text(
-                                        "${cards[index].serviceName} History Deleted!",
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    ],
-                                  ));
-                            }
-                          },
-                          background: Container(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 20.0),
-                            color: Colors.red,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Icon(
-                                  Icons.delete_rounded,
-                                  color: Colors.white,
-                                  size: 30.0,
-                                ),
-                              ],
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data.subscriptions.length,
+                        itemBuilder: (context, index) {
+                          return WidgetAnimator(
+                            child: SubscriptionHistoryCard(
+                              sId: snapshot
+                                  .data.subscriptions[index].subscriptionId,
+                              date: "10-15-2020",
+                              isPrem:
+                                  snapshot.data.subscriptions[index].isPremium,
+                              serviceName: snapshot
+                                  .data.subscriptions[index].serviceName,
                             ),
+                          );
+                        },
+                      );
+                    } else {
+                      return Container(
+                        height: MediaQuery.of(context).size.height * 0.55,
+                        child: Center(
+                          child: JumpingDotsProgressIndicator(
+                            fontSize: 30,
+                            color: Provider.of<ThemeProvider>(context).darkTheme
+                                ? Colors.white
+                                : kPrimaryBlueColor,
                           ),
                         ),
                       );
-                    }),
+                    }
+                  },
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _clearAllHistory() async {
+    customSnackBar(
+      context,
+      Colors.red,
+      Row(
+        children: const [
+          SizedBox(
+            height: 20.0,
+            width: 20.0,
+            child: CircularProgressIndicator(
+              strokeWidth: 1.5,
+              valueColor: AlwaysStoppedAnimation(Colors.white),
+            ),
+          ),
+          SizedBox(width: 8.0),
+          Text(
+            'Deleting History...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+
+    var value = await ServiceController().deleteSubscriptionHistoryService();
+    setState(() {});
+
+    if (value is String) {
+      customSnackBar(
+        context,
+        Colors.red,
+        Row(
+          children: [
+            const Icon(Icons.info, color: Colors.white),
+            const SizedBox(width: 8.0),
+            Text(
+              value,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      );
+    } else {
+      customSnackBar(
+        context,
+        kSecondaryBlueColor,
+        Row(
+          children: const [
+            Icon(Icons.check, color: Colors.white),
+            SizedBox(width: 8.0),
+            Text(
+              'History has been deleted!',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      );
+    }
   }
 }
