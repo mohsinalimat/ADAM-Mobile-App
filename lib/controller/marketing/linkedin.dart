8ec3b58b7f93a8b79543cb11cf1ad931c0b09f29
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2021-11-20 17:42:57 +0500

    linkedin scraper added

--- lib/controller/marketing/linkedin.dart
+++ lib/controller/marketing/linkedin.dart
@@ -1,21 +1,24 @@
-import 'package:adam/model/scraping/instagram/scraped_user.dart';
+import 'package:adam/model/scraping/linkedin/scraped_data.dart';
 import 'package:dio/dio.dart';
 
 class LinkedInMarketing {
   Dio dio = Dio();
+
   var headers = {
     "Accept": "application/json",
     "Access-Control-Allow-Origin": "*"
   };
-  Future scrapeUserData(String userLinkedIn, String passwordLinkedIn,
-      String targetUsername) async {
+
+  Future scrapeUserData(
+      String emailLinkedIn, String passwordLinkedIn, String keyword) async {
     try {
-      String url = "https://adam-web-api.herokuapp.com/instagram/check-target";
+      // String url = "http://10.0.2.2:5050/linkedin/scraper";
+      String url = "http://172.20.52.237:2020/linkedin/scraper";
 
       var body = {
-        // "username": userInsta,
-        // "password": passInsta,
-        "targeted_username": targetUsername,
+        "email": emailLinkedIn,
+        "password": passwordLinkedIn,
+        "keyword": keyword,
       };
 
       Response response = await dio.post(
@@ -27,7 +30,7 @@ class LinkedInMarketing {
       );
       if (response.statusCode == 200) {
         print("GOT THE DATA!");
-        return InstaScrapedUserList.fromJSON(response.data);
+        return LinkedInScrapedUserList.fromJSON(response.data);
       } else {
         return "Some error!";
       }
